c668e7c11deb2658ab61c4d71a016fef1e6df302
Vladimir Migashko <migashko@gmail.com>
2019-03-05 03:07:59 +0300

    fix

--- tests/prefixdb_suite.cpp
+++ tests/prefixdb_suite.cpp
@@ -1,11 +1,18 @@
 #include <fas/testing.hpp>
 #include <wfc/module/testing_domain.hpp>
+#include <package/prefixdb/prefixdb/prefixdb.hpp>
+
 
 namespace {
   
 UNIT(init, "")
 {
   using namespace fas::testing;
+  auto ptest = std::make_shared<wfc::testing_domain>();
+  wamba::prefixdb::prefixdb::domain_config conf;
+  auto pdemo = ptest->create<wamba::prefixdb::prefixdb>(conf);
+
+  
   t << nothing;
 }
 
